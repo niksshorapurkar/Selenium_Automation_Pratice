cfb977ea37ebb5aac96406a01ceec8134e55cdaa
Nikhil <nikhilshorapurkar@gmail.com>
2026-02-20 15:25:13 +0530

    anagram code

---
+++ code_pratice_python/anagram_code.py
@@ -0,0 +1,10 @@
+def anagram():
+    word1 = input("Enter the first word: ")
+    word2 = input("Enter the second word: ")
+
+    if sorted(word1) == sorted(word2):
+        print("The words are anagrams.")
+    else:
+        print("The words are not anagrams.")
+
+if __name__ == "__main__":    anagram()
